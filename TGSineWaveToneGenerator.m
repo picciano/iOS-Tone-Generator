c244b14ffe1ff70395b498d9225306dfca4a9815
Anthony Picciano <anthony.picciano@gmail.com>
2013-06-12 15:19:34 -0500

    Create TGSineWaveToneGenerator.m

---
+++ TGSineWaveToneGenerator.m
@@ -0,0 +1,171 @@
+//
+//  TGSineWaveToneGenerator.m
+//  Tone Generator
+//
+//  Created by Anthony Picciano on 6/12/13.
+//  Copyright (c) 2013 Anthony Picciano. All rights reserved.
+//
+
+#import "TGSineWaveToneGenerator.h"
+#import <AudioToolbox/AudioToolbox.h>
+
+OSStatus RenderTone(
+                    void *inRefCon,
+                    AudioUnitRenderActionFlags   *ioActionFlags,
+                    const AudioTimeStamp 		*inTimeStamp,
+                    UInt32 						inBusNumber,
+                    UInt32 						inNumberFrames,
+                    AudioBufferList 			*ioData)
+
+{
+	// Get the tone parameters out of the object
+	TGSineWaveToneGenerator *toneGenerator =
+    (__bridge TGSineWaveToneGenerator *)inRefCon;
+	double theta = toneGenerator->theta;
+    double amplitude = toneGenerator->amplitude;
+	double theta_increment = 2.0 * M_PI * toneGenerator->frequency / toneGenerator->sampleRate;
+    
+	// This is a mono tone generator so we only need the first buffer
+	const int channel = 0;
+	Float32 *buffer = (Float32 *)ioData->mBuffers[channel].mData;
+	
+	// Generate the samples
+	for (UInt32 frame = 0; frame < inNumberFrames; frame++)
+	{
+		buffer[frame] = sin(theta) * amplitude;
+		
+		theta += theta_increment;
+		if (theta > 2.0 * M_PI)
+		{
+			theta -= 2.0 * M_PI;
+		}
+	}
+	
+	// Store the theta back in the view controller
+	toneGenerator->theta = theta;
+    
+	return noErr;
+}
+
+void ToneInterruptionListener(void *inClientData, UInt32 inInterruptionState)
+{
+	TGSineWaveToneGenerator *toneGenerator =
+    (__bridge TGSineWaveToneGenerator *)inClientData;
+	
+	[toneGenerator stop];
+}
+
+@implementation TGSineWaveToneGenerator
+
+- (id)init
+{
+    return [self initWithFrequency:SINE_WAVE_TONE_GENERATOR_FREQUENCY_DEFAULT amplitude:SINE_WAVE_TONE_GENERATOR_AMPLITUDE_DEFAULT];
+}
+
+- (id)initWithFrequency:(double)hertz amplitude:(double)volume
+{
+    if (self = [super init]) {
+        frequency = hertz;
+        amplitude = volume;
+        sampleRate = SINE_WAVE_TONE_GENERATOR_SAMPLE_RATE_DEFAULT;
+        
+        OSStatus result = AudioSessionInitialize(NULL, NULL, ToneInterruptionListener, (__bridge void *)(self));
+        if (result == kAudioSessionNoError)
+        {
+            UInt32 sessionCategory = kAudioSessionCategory_MediaPlayback;
+            AudioSessionSetProperty(kAudioSessionProperty_AudioCategory, sizeof(sessionCategory), &sessionCategory);
+        }
+        AudioSessionSetActive(true);
+    }
+    
+    return self;
+}
+
+- (void)playForDuration:(float)time
+{
+    [self play];
+    [self performSelector:@selector(stop) withObject:nil afterDelay:time];
+}
+
+- (void)play
+{
+    if (!toneUnit)
+	{
+		[self createToneUnit];
+		
+		// Stop changing parameters on the unit
+		OSErr err = AudioUnitInitialize(toneUnit);
+		NSAssert1(err == noErr, @"Error initializing unit: %hd", err);
+		
+		// Start playback
+		err = AudioOutputUnitStart(toneUnit);
+		NSAssert1(err == noErr, @"Error starting unit: %hd", err);
+	}
+}
+
+- (void)stop
+{
+    if (toneUnit)
+	{
+		AudioOutputUnitStop(toneUnit);
+		AudioUnitUninitialize(toneUnit);
+		AudioComponentInstanceDispose(toneUnit);
+		toneUnit = nil;
+	}
+}
+
+- (void)createToneUnit
+{
+	// Configure the search parameters to find the default playback output unit
+	// (called the kAudioUnitSubType_RemoteIO on iOS but
+	// kAudioUnitSubType_DefaultOutput on Mac OS X)
+	AudioComponentDescription defaultOutputDescription;
+	defaultOutputDescription.componentType = kAudioUnitType_Output;
+	defaultOutputDescription.componentSubType = kAudioUnitSubType_RemoteIO;
+	defaultOutputDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
+	defaultOutputDescription.componentFlags = 0;
+	defaultOutputDescription.componentFlagsMask = 0;
+	
+	// Get the default playback output unit
+	AudioComponent defaultOutput = AudioComponentFindNext(NULL, &defaultOutputDescription);
+	NSAssert(defaultOutput, @"Can't find default output");
+	
+	// Create a new unit based on this that we'll use for output
+	OSErr err = AudioComponentInstanceNew(defaultOutput, &toneUnit);
+	NSAssert1(toneUnit, @"Error creating unit: %hd", err);
+	
+	// Set our tone rendering function on the unit
+	AURenderCallbackStruct input;
+	input.inputProc = RenderTone;
+	input.inputProcRefCon = (__bridge void *)(self);
+	err = AudioUnitSetProperty(toneUnit,
+                               kAudioUnitProperty_SetRenderCallback,
+                               kAudioUnitScope_Input,
+                               0,
+                               &input,
+                               sizeof(input));
+	NSAssert1(err == noErr, @"Error setting callback: %hd", err);
+	
+	// Set the format to 32 bit, single channel, floating point, linear PCM
+	const int four_bytes_per_float = 4;
+	const int eight_bits_per_byte = 8;
+	AudioStreamBasicDescription streamFormat;
+	streamFormat.mSampleRate = sampleRate;
+	streamFormat.mFormatID = kAudioFormatLinearPCM;
+	streamFormat.mFormatFlags =
+    kAudioFormatFlagsNativeFloatPacked | kAudioFormatFlagIsNonInterleaved;
+	streamFormat.mBytesPerPacket = four_bytes_per_float;
+	streamFormat.mFramesPerPacket = 1;
+	streamFormat.mBytesPerFrame = four_bytes_per_float;
+	streamFormat.mChannelsPerFrame = 1;
+	streamFormat.mBitsPerChannel = four_bytes_per_float * eight_bits_per_byte;
+	err = AudioUnitSetProperty (toneUnit,
+                                kAudioUnitProperty_StreamFormat,
+                                kAudioUnitScope_Input,
+                                0,
+                                &streamFormat,
+                                sizeof(AudioStreamBasicDescription));
+	NSAssert1(err == noErr, @"Error setting stream format: %hd", err);
+}
+
+@end
